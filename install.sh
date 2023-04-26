7ec53f6776ba56fc985d5936657c3495ddea2a11
Neil Livingstone <nlivingstone@users.noreply.github.com>
2023-04-26 13:06:03 -0400

    WIP

--- install.sh
+++ install.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/bash
 
-cp -r $PWD/wallpaper ~/Wallpaper
+WALLPAPER_DIR="$HOME/Wallpaper"
+if [ ! -d $WALLPAPER_DIR ]; then	
+	echo "$WALLPAPER_DIR : File DOES NOT exist... Create symlink."
+	ln -s $PWD/wallpaper $WALLPAPER_DIR
+else
+	echo "$WALLPAPER_DIR : File exists... Skipping."
 
-ln -s $PWD/scripts ~/.scripts
-ln -s $PWD/config/hypr ~/.config/hypr
+fi
 
+SCRIPTS_DIR='~/.scripts'
+if [ ! -L ${SCRIPTS_DIR}/ ]; then
+	echo "Creating symlink for scripts dir"
+#	ln -s $PWD/scripts ~/.scripts
+else
+	echo "$SCRIPTS_DIR already exists. Skipping."
+fi
+
+HYPRLAND_DIR='~/.config/hypr'
+if [ ! -L ${HYPRLAND_DIR} ]; then
+	echo "Creating symlink for hyprland config dir"
+#	ln -s $PWD/config/hypr ~/.config/hypr
+else 
+	echo "$HYPRLAND_DIR already exists. Skipping."
+fi
